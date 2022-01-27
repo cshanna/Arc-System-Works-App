a811352af23f7b2a00222ca95e9f3af161191f56
Chris <cshanna14@gmail.com>
2022-01-27 01:00:54 -0500

    Added navbar & carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './navigation/carousel.component';
 import { CategoryNavBarComponent } from './navigation/categorynavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryNavBarComponent
+    CategoryNavBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
