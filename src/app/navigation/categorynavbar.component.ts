01d92c97e89a6d3e7eada5bc383db92f1b80185e
Chris <cshanna14@gmail.com>
2022-01-26 22:47:14 -0500

    Set up example component.

---
+++ src/app/navigation/categorynavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-categorynavbar',
+    templateUrl: 'categorynavbar.component.html',
+    styleUrls: ['categorynavbar.component.css']
+})
+export class CategoryNavBarComponent {
+
+}
